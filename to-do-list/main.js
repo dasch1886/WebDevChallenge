dee136b8500eed83f46e385eaf6cde415a7253af
Kamil Kopacz <k.dasch@interia.pl>
2018-09-13 21:07:31 +0200

    little fix in add and remove task

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -1,9 +1,5 @@
-let Task = function(title, category, value, date) {
-  this.title = title;
-  this.category = category;
-  this.value = value;
-  this.date = date;
-};
+let tasks = [];
+let id = [];
 
 const months = [
   "January",
@@ -20,20 +16,33 @@ const months = [
   "December"
 ];
 
-const Tasks = [];
+let Task = function(title, category, value, date) {
+  this.id = null;
+  this.title = title;
+  this.category = category;
+  this.value = value;
+  this.date = null;
 
-let getDate = function(date) {
-  let day = date.getDate();
-  let month = date.getMonth();
-  let year = date.getFullYear();
-  let fulldate = null;
-  if (day < 10) {
-    fulldate = "0" + day + " " + months[month] + " " + year;
-  } else {
-    fulldate = day + " " + months[month] + " " + year;
+  this.setId = function(){
+    if(id.length === 0){
+      this.id = 1;
+      id.push(this.id);
+    }else{
+      this.id = Math.max.apply(Math,id)+1;
+      id.push(this.id);
+    }
   }
 
-  return fulldate;
+  this.setDate = function(date) {
+    if (date.getDate() < 10) {
+      this.date = "0" + date.getDate()+ " " + months[date.getMonth()] + " " + date.getFullYear();
+    } else {
+      this.date =  date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
+    }
+  };
+
+  this.setId();
+  this.setDate(date);
 };
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -46,18 +55,18 @@ document.addEventListener("DOMContentLoaded", function() {
   taskCreator.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    let date = new Date();
-
     if (
       input.value !== "" &&
         select.value !== "Select" &&
         textarea.value !== ""
     ) {
-      task = new Task(input.value, select.value, textarea.value, getDate(date));
+      tasks.push(new Task(input.value, select.value, textarea.value, new Date()));
 
       let div = document.createElement("div");
       div.classList.add("box");
       div.classList.add("item__task");
+      div.setAttribute('data-id',tasks[tasks.length-1].id);
+      div.setAttribute('data-category',tasks[tasks.length-1].category);
 
       let divHeader = document.createElement("div");
       divHeader.classList.add("task__header");
@@ -73,27 +82,38 @@ document.addEventListener("DOMContentLoaded", function() {
       divHeader.appendChild(i);
 
 
-      divHeader.children[0].textContent = task.date;
-      divHeader.children[1].textContent = "Category: " + task.category;
-      divHeader.children[2].textContent = "Title: " + task.title;
+      divHeader.children[0].textContent = tasks[tasks.length-1].date;
+      divHeader.children[1].textContent = "Category: " + tasks[tasks.length-1].category;
+      divHeader.children[2].textContent = "Title: " + tasks[tasks.length-1].title;
 
       let divContent = document.createElement("div");
       divContent.classList.add("task__content");
-      divContent.textContent = task.value;
+      divContent.textContent = tasks[tasks.length-1].value;
 
       div.appendChild(divHeader);
       div.appendChild(divContent);
 
       taskList.appendChild(div);
 
-      Tasks.push(task);
+      console.log(tasks,id);
     }
   });
 
   taskList.addEventListener('click',function(e){
     if(e.target.className.search('task__close')!=-1){
         const task = e.target.closest('.item__task');
+        const idTask = parseInt(task.getAttribute('data-id'),10);
+        
+        console.log(typeof(idTask));
+        tasks = tasks.filter(function(e){
+          return e.id !== idTask;
+        });
+
+        id = id.filter(function(e){
+          return e !== idTask;
+        });
         task.parentNode.removeChild(task);
+        console.log(tasks,id);
     }
   });
 });
